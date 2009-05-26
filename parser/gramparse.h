27bb08282486161074ae092e4fd3a2a0441b9594
paul cannon <paul@mozy.com>
2009-05-26 17:36:24 -0600

    dummy TYPECAST token type

--- parser/gramparse.h
+++ parser/gramparse.h
@@ -1,12 +1,13 @@
 #ifndef dummy_gramparse_h
 #define dummy_gramparse_h
 
-#define BCONST 1
-#define XCONST 2
-#define ICONST 3
-#define FCONST 4
-#define SCONST 5
-#define PARAM  10
-#define IDENT  11
+#define BCONST   1
+#define XCONST   2
+#define ICONST   3
+#define FCONST   4
+#define SCONST   5
+#define TYPECAST 9
+#define PARAM    10
+#define IDENT    11
 
 #endif
