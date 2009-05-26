cd90af458691ebf09a645243fd0f44a7c863e771
paul cannon <paul@mozy.com>
2009-05-26 17:27:15 -0600

    dummy BCONST and a few other tokentypes

---
+++ parser/gramparse.h
@@ -0,0 +1,12 @@
+#ifndef dummy_gramparse_h
+#define dummy_gramparse_h
+
+#define BCONST 1
+#define XCONST 2
+#define ICONST 3
+#define FCONST 4
+#define SCONST 5
+#define PARAM  10
+#define IDENT  11
+
+#endif
