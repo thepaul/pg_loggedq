e8926bb2707c0b07db029fa5bf8eae0fe856d5a4
paul cannon <paul@mozy.com>
2009-05-25 21:19:53 -0600

    combine contiguous whitespace
    we could find contiguous whitespace when there is a comment in
    between other sections of whitespace.

--- parse_sql.py
+++ parse_sql.py
@@ -17,13 +17,23 @@ yylex.restype = c_int
 yylval = c_char_p.in_dll(scan_so, 'yylval')
 
 def tokenize_sql(sql):
-    scanner_init(sql)
+    scanner_init(sql.strip())
+    last_was_whitespace = True
     while True:
         result = yylex()
         if result == 0:
             break
         elif result == 1:
+            last_was_whitespace = False
             yield '?'
         elif result == 2:
+            val = yylval.value
+            if val.isspace():
+                if last_was_whitespace:
+                    continue
+                else:
+                    last_was_whitespace = True
+            else:
+                last_was_whitespace = False
             yield yylval.value
     scanner_finish()
