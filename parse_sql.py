2a82b59a9c760414881699ff8a3119b1efa949af
paul cannon <paul@mozy.com>
2009-05-26 16:47:04 -0600

    use new reentrant scanner C functions
    also take whitespace consolidation out of this lowest-level tokenizer.
    we'll build that on a higher level or a wrapper.

--- parse_sql.py
+++ parse_sql.py
@@ -2,38 +2,48 @@ from ctypes import *
 
 scan_so = CDLL('./scan.so')
 
+class PerScannerData(Structure):
+    _fields_ = [('internal0', c_int),
+                ('internal1', c_char_p),
+                ('internal2', c_int),
+                ('lval', c_char_p)]
+
+yylex_init_extra = scan_so.base_yylex_init_extra
+yylex_init_extra.argtypes = [POINTER(PerScannerData), POINTER(c_void_p)]
+yylex_init_extra.restype = c_int
+
+yylex_destroy = scan_so.base_yylex_destroy
+yylex_destroy.argtypes = [c_void_p]
+yylex_destroy.restype = c_int
+
 scanner_init = scan_so.scanner_init
-scanner_init.argtypes = [c_char_p]
+scanner_init.argtypes = [c_char_p, c_void_p]
 scanner_init.restype = None
 
 scanner_finish = scan_so.scanner_finish
-scanner_finish.argtypes = []
+scanner_finish.argtypes = [c_void_p]
 scanner_finish.restype = None
 
 yylex = scan_so.base_yylex
-yylex.argtypes = []
+yylex.argtypes = [c_char_p, c_void_p]
 yylex.restype = c_int
 
-yylval = c_char_p.in_dll(scan_so, 'yylval')
-
 def tokenize_sql(sql):
-    scanner_init(sql.strip())
-    last_was_whitespace = True
-    while True:
-        result = yylex()
-        if result == 0:
-            break
-        elif result == 1:
-            last_was_whitespace = False
-            yield '?'
-        elif result == 2:
-            val = yylval.value
-            if val.isspace():
-                if last_was_whitespace:
-                    continue
-                else:
-                    last_was_whitespace = True
-            else:
-                last_was_whitespace = False
-            yield yylval.value
-    scanner_finish()
+    dat = PerScannerData()
+    scanner = c_void_p()
+    yylex_init_extra(pointer(dat), pointer(scanner))
+    try:
+        scanner_init(sql.strip(), scanner)
+        try:
+            while True:
+                result = yylex("", scanner)
+                if result == 0:
+                    break
+                elif result == 1:
+                    yield '?'
+                elif result == 2:
+                    yield dat.lval
+       finally:
+            scanner_finish(scanner)
+    finally:
+        yylex_destroy(scanner)
