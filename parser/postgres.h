5543d72d4b39bfc440cac145af76b9ea0cb921af
paul cannon <paul@mozy.com>
2009-05-26 18:46:32 -0600

    remaining dummy stuff. compiles cleanly now.

--- parser/postgres.h
+++ parser/postgres.h
@@ -25,6 +25,17 @@ YYSTYPE yylval;
 
 typedef size_t Size;
 
+typedef struct {
+   char* name;
+   int value;
+} ScanKeyword;
+
+ScanKeyword* ScanKeywordLookup(const char* name);
+void report_error(int code, const char* msg, ...);
+const char* errmsg(const char* fmt, ...);
+void truncate_identifier(char* str, int len, int mongoose);
+char* downcase_truncate_identifier(const char* str, int len, int mongoose);
+
 /* pg default is 64, but we want to accept everything we can */
 #define NAMEDATALEN 1024
 
@@ -36,6 +47,9 @@ typedef size_t Size;
 #define pfree(x) (free(x))
 #define repalloc(b,n) (realloc(b,n))
 
+#define pg_verifymbstr(a,b,c) ((void)0)
+#define pg_mbstrlen_with_len(s,n) strlen(s)
+
 #include <assert.h>
 #define Assert assert
 
@@ -43,6 +57,15 @@ typedef size_t Size;
 
 #define ERRCODE_NONSTANDARD_USE_OF_ESCAPE_CHARACTER 1
 #define ERRCODE_SYNTAX_ERROR                        2
+#define ERRCODE_INTERNAL                            3
+
+#define IS_HIGHBIT_SET(c) ((c) & 0x80)
+
+#define ereport(errtype,erest) (report_error erest)
+#define errcode(s) (s)
+#define errhint(s) (s)
+#define errposition(p) (p)
+#define errmsg_internal(fmt,arg) errcode(ERRCODE_INTERNAL),errmsg(fmt,arg)
 
 #define _(a) (a)
 
