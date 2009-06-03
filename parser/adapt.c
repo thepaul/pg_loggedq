0b26941985051d6688f8b1b4a82251a5f75ceb29
paul cannon <paul@mozy.com>
2009-06-02 18:43:34 -0600

    malloc per_scanner_data instance
    instead of returning off the stack! what am I, a retard?

--- parser/adapt.c
+++ parser/adapt.c
@@ -16,24 +16,25 @@ int seen_error = 0;
 yyscan_t
 lexer_init(const char* sql)
 {
-    per_scanner_data extra;
+    per_scanner_data *extra;
     yyscan_t scanner;
-    int rc;
 
-    extra._xcdepth = 0;
-    extra._dolqstart = NULL;
-    extra._backslash_quote = BACKSLASH_QUOTE_SAFE_ENCODING;
-    extra._escape_string_warning = false;
-    extra._standard_conforming_strings = false;
-    extra._warn_on_first_escape = false;
-    extra._saw_high_bit = false;
-    extra._literalbuf = NULL;
-    extra._literallen = 0;
-    extra._literalalloc = 0;
-    extra._scanbufhandle = NULL;
-    extra._scanbuf = NULL;
+    extra = (per_scanner_data*) calloc(sizeof(per_scanner_data), 1);
 
-    if (base_yylex_init_extra(&extra, &scanner) != 0)
+    extra->_xcdepth = 0;
+    extra->_dolqstart = NULL;
+    extra->_backslash_quote = BACKSLASH_QUOTE_SAFE_ENCODING;
+    extra->_escape_string_warning = false;
+    extra->_standard_conforming_strings = false;
+    extra->_warn_on_first_escape = false;
+    extra->_saw_high_bit = false;
+    extra->_literalbuf = NULL;
+    extra->_literallen = 0;
+    extra->_literalalloc = 0;
+    extra->_scanbufhandle = NULL;
+    extra->_scanbuf = NULL;
+
+    if (base_yylex_init_extra(extra, &scanner) != 0)
         return NULL;
     scanner_init(sql, scanner);
     return scanner;
@@ -42,8 +43,10 @@ lexer_init(const char* sql)
 void
 lexer_destroy(yyscan_t scanner)
 {
+    void* extra = base_yyget_extra(scanner);
     scanner_finish(scanner);
     base_yylex_destroy(scanner);
+    free(extra);
 }
 
 char*
