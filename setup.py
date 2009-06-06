eb22ee86835f2745fa222f9344553133cf1d5001
paul cannon <paul@mozy.com>
2009-06-06 13:37:37 -0600

    metainfo in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from distutils.core import setup, Extension
 
 setup(name='pg_loggedq',
       version='0.2.0',
+      description='Processing of PostgreSQL logged queries',
+      author='paul cannon',
+      author_email='paul@mozy.com',
       packages=['pg_loggedq'],
-      ext_modules=[Extension('pg_loggedq._parsesql',
-                             sources=['_parsesqlmodule.c'],
-                             libraries=['sqlscanner'],
-                             library_dirs=['.'])])
+      ext_modules=[
+          Extension('pg_loggedq._parsesql',
+              sources=['_parsesqlmodule.c'],
+              libraries=['sqlscanner'],
+              library_dirs=['.'])])
